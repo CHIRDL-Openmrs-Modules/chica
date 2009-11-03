4bab1ff473d7715242f1e7fdb238920dc378d0b3
Sheley, Meena Ellen <msheley@iu.edu>
2009-11-03 18:29:19 +0000

    --chica  * script that adds hl7 location changes in chica_location_tag_attribute chica_location_tag_attribute_value

---
+++ metadata/upgrade_scripts/chica_1_44_22_to_1_44_23.sql
@@ -0,0 +1,76 @@
+insert into chica_location_tag_attribute (name, description)
+values ('HL7ConfigFile', 'Configuration file for outbound hl7 messages');
+
+insert into chica_location_tag_attribute_value (location_tag_id, value,
+location_tag_attribute_id, location_id)
+values(
+(select distinct location_tag_id from location_tag where tag = 'PEREG3'),
+'f:\\chica\\config\\PEREG3\\hl7_config_pecar.xml',
+(select location_tag_attribute_id from chica_location_tag_attribute
+where name = 'HL7ConfigFile'),
+(select location_id from location where name = 'PEPS'));
+
+insert into chica_location_tag_attribute_value (location_tag_id, value,
+location_tag_attribute_id, location_id)
+values(
+(select distinct location_tag_id from location_tag where tag = 'PEREG2'),
+'f:\\chica\\config\\PEREG2\\hl7_config_pecar.xml',
+(select location_tag_attribute_id from chica_location_tag_attribute
+where name = 'HL7ConfigFile'),
+(select location_id from location where name = 'PEPS'));
+
+insert into chica_location_tag_attribute_value (location_tag_id, value,
+location_tag_attribute_id, location_id)
+values(
+(select distinct location_tag_id from location_tag where tag = 'PEREG1'),
+'f:\\chica\\config\\PEREG1\\hl7_config_pecar.xml',
+(select location_tag_attribute_id from chica_location_tag_attribute
+where name = 'HL7ConfigFile'),
+(select location_id from location where name = 'PEPS'));
+
+insert into chica_location_tag_attribute_value (location_tag_id, value,
+location_tag_attribute_id, location_id)
+values(
+(select distinct location_tag_id from location_tag where tag = 'PEREG4'),
+'f:\\chica\\config\\PEREG4\\hl7_config_pecar.xml',
+(select location_tag_attribute_id from chica_location_tag_attribute
+where name = 'HL7ConfigFile'),
+(select location_id from location where name = 'PEPS'));
+
+insert into chica_location_tag_attribute_value (location_tag_id, value,
+location_tag_attribute_id, location_id)
+values(
+(select distinct location_tag_id from location_tag where tag = 'Other - PECAR'),
+'f:\\chica\\config\\other_pecar\\hl7_config_pecar.xml',
+(select location_tag_attribute_id from chica_location_tag_attribute
+where name = 'HL7ConfigFile'),
+(select location_id from location where name = 'PEPS'));
+
+
+insert into chica_location_tag_attribute_value (location_tag_id, value,
+location_tag_attribute_id, location_id)
+values(
+(select distinct location_tag_id from location_tag where tag = 'PED1'),
+'f:\\chica\\config\\PED1\\hl7_config_pcc.xml',
+(select location_tag_attribute_id from chica_location_tag_attribute
+where name = 'HL7ConfigFile'),
+(select location_id from location where name = 'PCPS'));
+
+insert into chica_location_tag_attribute_value (location_tag_id, value,
+location_tag_attribute_id, location_id)
+values(
+(select distinct location_tag_id from location_tag where tag = 'PED2'),
+'f:\\chica\\config\\PED2\\hl7_config_pcc.xml',
+(select location_tag_attribute_id from chica_location_tag_attribute
+where name = 'HL7ConfigFile'),
+(select location_id from location where name = 'PCPS'));
+
+insert into chica_location_tag_attribute_value (location_tag_id, value,
+location_tag_attribute_id, location_id)
+values(
+(select distinct location_tag_id from location_tag where tag = 'Other - PCC'),
+'f:\\chica\\config\\other_pcc\\hl7_config_pcc.xml',
+(select location_tag_attribute_id from chica_location_tag_attribute
+where name = 'HL7ConfigFile'),
+(select location_id from location where name = 'PCPS'));
+
