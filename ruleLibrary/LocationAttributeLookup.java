31b978d2400d066dad5752ce9d29d39a8c1e7cd1
Dugan, Tammy <tmdugan@iu.edu>
2010-07-13 19:43:58 +0000

    chica
    * add initial ASQ rules

---
+++ ruleLibrary/LocationAttributeLookup.java
@@ -0,0 +1,85 @@
+package org.openmrs.module.chica.rule;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.openmrs.Patient;
+import org.openmrs.api.context.Context;
+import org.openmrs.logic.LogicContext;
+import org.openmrs.logic.LogicCriteria;
+import org.openmrs.logic.LogicException;
+import org.openmrs.logic.LogicService;
+import org.openmrs.logic.Rule;
+import org.openmrs.logic.op.Operator;
+import org.openmrs.logic.result.Result;
+import org.openmrs.logic.result.Result.Datatype;
+import org.openmrs.logic.rule.RuleParameterInfo;
+import org.openmrs.module.atd.hibernateBeans.FormInstance;
+import org.openmrs.module.chirdlutil.hibernateBeans.LocationAttributeValue;
+import org.openmrs.module.chirdlutil.service.ChirdlUtilService;
+import org.openmrs.module.chica.service.ChicaService;
+
+public class LocationAttributeLookup implements Rule
+{
+	private LogicService logicService = Context.getLogicService();
+
+	/**
+	 * *
+	 * 
+	 * @see org.openmrs.logic.rule.Rule#getParameterList()
+	 */
+	public Set<RuleParameterInfo> getParameterList()
+	{
+		return null;
+	}
+
+	/**
+	 * *
+	 * 
+	 * @see org.openmrs.logic.rule.Rule#getDependencies()
+	 */
+	public String[] getDependencies()
+	{
+		return new String[]
+		{};
+	}
+
+	/**
+	 * *
+	 * 
+	 * @see org.openmrs.logic.rule.Rule#getTTL()
+	 */
+	public int getTTL()
+	{
+		return 0; // 60 * 30; // 30 minutes
+	}
+
+	/**
+	 * *
+	 * 
+	 * @see org.openmrs.logic.rule.Rule#getDatatype(String)
+	 */
+	public Datatype getDefaultDatatype()
+	{
+		return Datatype.CODED;
+	}
+	public Result eval(LogicContext context, Patient patient,
+			Map<String, Object> parameters) throws LogicException
+	{
+		FormInstance formInstance = (FormInstance) parameters
+			.get("formInstance");
+		ChirdlUtilService chirdlUtilService = Context.getService(ChirdlUtilService.class);
+		String param = (String) parameters.get("param1");
+		if (formInstance != null && param!=null)
+		{
+			Integer locationId = formInstance.getLocationId();
+			LocationAttributeValue locationAttributeValue = 
+				chirdlUtilService.getLocationAttributeValue(locationId, param);
+		
+			if(locationAttributeValue != null){
+				return new Result(locationAttributeValue.getValue());
+			}
+		}
+		return Result.emptyResult();
+	}
+}
