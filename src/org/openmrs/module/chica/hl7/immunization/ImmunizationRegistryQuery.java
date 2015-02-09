8f68d2df6d30285ec6070f9455c7a410c8022bdf
msheley <msheley@iu.edu>
2015-02-09 15:13:41 -0500

    Minor format adjustment

--- src/org/openmrs/module/chica/hl7/immunization/ImmunizationRegistryQuery.java
+++ src/org/openmrs/module/chica/hl7/immunization/ImmunizationRegistryQuery.java
@@ -314,17 +314,17 @@ public class ImmunizationRegistryQuery
 							if (obxCode.equals(LOINC_DOSE_NUMBER_CODE)) {
 								if (obsValueType.equals("NM")) {
 									
-										NM data = (NM) obxValue;
-										String dataString = data.getValue();
-										try {
-											if (dataString != null && forecastedImmunization != null) {
-												forecastedImmunization.setDose(Integer
-													.parseInt(dataString));
-											}
-										} catch (NumberFormatException  e) {
-											log.error("Invalid number format for Dose number: " + dataString);
-											forecastedImmunization.setDose(0);
+									NM data = (NM) obxValue;
+									String dataString = data.getValue();
+									try {
+										if (dataString != null && forecastedImmunization != null) {
+											forecastedImmunization.setDose(Integer
+												.parseInt(dataString));
 										}
+									} catch (NumberFormatException  e) {
+										log.error("Invalid number format for Dose number: " + dataString);
+										forecastedImmunization.setDose(0);
+									}
 									
 									
 								}
