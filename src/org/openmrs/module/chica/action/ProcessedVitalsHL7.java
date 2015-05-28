46b7d6f0c0dd1685c53c63f49f5c56884e0ed261
sjmckee <sjmckee@iu.edu>
2015-05-28 10:53:57 -0400

    New action class for the Processed Vitals HL7 state.

---
+++ src/org/openmrs/module/chica/action/ProcessedVitalsHL7.java
@@ -0,0 +1,60 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.chica.action;
+
+import java.util.HashMap;
+
+import org.openmrs.Patient;
+import org.openmrs.module.chirdlutilbackports.BaseStateActionHandler;
+import org.openmrs.module.chirdlutilbackports.StateManager;
+import org.openmrs.module.chirdlutilbackports.action.ProcessStateAction;
+import org.openmrs.module.chirdlutilbackports.hibernateBeans.PatientState;
+import org.openmrs.module.chirdlutilbackports.hibernateBeans.State;
+import org.openmrs.module.chirdlutilbackports.hibernateBeans.StateAction;
+
+
+/**
+ * Action class for supporting the "Processed Vitals HL7" state.
+ * 
+ * @author Steve McKee
+ */
+public class ProcessedVitalsHL7 implements ProcessStateAction {
+	
+	/**
+	 * @see org.openmrs.module.chirdlutilbackports.action.ProcessStateAction#changeState(org.openmrs.module.chirdlutilbackports.hibernateBeans.PatientState, java.util.HashMap)
+	 */
+	@Override
+	public void changeState(PatientState patientState, HashMap<String, Object> parameters) {
+		// Deliberately empty because processAction changes the state
+	}
+	
+	/**
+	 * @see org.openmrs.module.chirdlutilbackports.action.ProcessStateAction#processAction(org.openmrs.module.chirdlutilbackports.hibernateBeans.StateAction, org.openmrs.Patient, org.openmrs.module.chirdlutilbackports.hibernateBeans.PatientState, java.util.HashMap)
+	 */
+	@Override
+	public void processAction(StateAction stateAction, Patient patient, PatientState patientState,
+	                          HashMap<String, Object> parameters) {
+		// This action currently doesn't do much, but it's a placeholder in case future enhancements are performed
+		Integer sessionId = patientState.getSessionId();
+		State currState = patientState.getState();
+		Integer locationTagId = patientState.getLocationTagId();
+		Integer locationId = patientState.getLocationId();
+		
+		StateManager.endState(patientState);
+		
+		BaseStateActionHandler.changeState(patient, sessionId, currState,
+				stateAction, parameters, locationTagId, locationId);
+	}
+	
+}
