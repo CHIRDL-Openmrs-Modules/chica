c9782524d8a1ef904e4ebea7fa379b7fb0d0ca92
tmdugan <tmdugan@iu.edu>
2015-06-03 15:32:36 -0400

    Converted to message type to MDM_T02 to match example from IU Health

--- src/org/openmrs/module/chica/action/ExportPowerNote.java
+++ src/org/openmrs/module/chica/action/ExportPowerNote.java
@@ -15,6 +15,7 @@ package org.openmrs.module.chica.action;
 
 import java.io.BufferedReader;
 import java.io.StringReader;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,6 +23,7 @@ import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Patient;
+import org.openmrs.PatientIdentifier;
 import org.openmrs.api.context.Context;
 import org.openmrs.logic.result.Result;
 import org.openmrs.module.chica.hibernateBeans.Encounter;
@@ -35,9 +37,13 @@ import org.openmrs.module.chirdlutilbackports.hibernateBeans.StateAction;
 import org.openmrs.module.chirdlutilbackports.service.ChirdlUtilBackportsService;
 import org.openmrs.module.dss.hibernateBeans.Rule;
 import org.openmrs.module.dss.service.DssService;
-import org.openmrs.module.sockethl7listener.HL7MessageConstructor;
 
+import ca.uhn.hl7v2.model.v25.datatype.TX;
+import ca.uhn.hl7v2.model.v25.message.MDM_T02;
+import ca.uhn.hl7v2.model.v25.segment.MSH;
 import ca.uhn.hl7v2.model.v25.segment.OBX;
+import ca.uhn.hl7v2.model.v25.segment.PID;
+import ca.uhn.hl7v2.parser.PipeParser;
 
 /**
  * Action class to initiate export of the physician note to the Cerner PowerNote.
@@ -55,14 +61,17 @@ public class ExportPowerNote implements ProcessStateAction {
 	private Log log = LogFactory.getLog(this.getClass());
 	
 	/**
-	 * @see org.openmrs.module.chirdlutilbackports.action.ProcessStateAction#changeState(org.openmrs.module.chirdlutilbackports.hibernateBeans.PatientState, java.util.HashMap)
+	 * @see org.openmrs.module.chirdlutilbackports.action.ProcessStateAction#changeState(org.openmrs.module.chirdlutilbackports.hibernateBeans.PatientState,
+	 *      java.util.HashMap)
 	 */
 	public void changeState(PatientState patientState, HashMap<String, Object> parameters) {
 		// Deliberately empty because processAction changes the state
 	}
 	
 	/**
-	 * @see org.openmrs.module.chirdlutilbackports.action.ProcessStateAction#processAction(org.openmrs.module.chirdlutilbackports.hibernateBeans.StateAction, org.openmrs.Patient, org.openmrs.module.chirdlutilbackports.hibernateBeans.PatientState, java.util.HashMap)
+	 * @see org.openmrs.module.chirdlutilbackports.action.ProcessStateAction#processAction(org.openmrs.module.chirdlutilbackports.hibernateBeans.StateAction,
+	 *      org.openmrs.Patient, org.openmrs.module.chirdlutilbackports.hibernateBeans.PatientState,
+	 *      java.util.HashMap)
 	 */
 	public void processAction(StateAction stateAction, Patient patient, PatientState patientState,
 	                          HashMap<String, Object> parameters) {
@@ -112,14 +121,10 @@ public class ExportPowerNote implements ProcessStateAction {
 			
 			Encounter openmrsEncounter = (Encounter) encounterService.getEncounter(encounterId);
 			
-			HL7MessageConstructor constructor = new HL7MessageConstructor();
+			MDM_T02 mdm = new MDM_T02();
 			
-			constructor.AddSegmentMSH(openmrsEncounter);
-			constructor.AddSegmentPID(openmrsEncounter.getPatient());
-			
-			// Create OBR and OBX segments for Vitals
-			int orderRep = 0;
-			Date dateTime = openmrsEncounter.getEncounterDatetime();
+			addSegmentMSH(openmrsEncounter, mdm);
+			addSegmentPID(openmrsEncounter.getPatient(), mdm);
 			
 			BufferedReader reader = null;
 			
@@ -128,9 +133,9 @@ public class ExportPowerNote implements ProcessStateAction {
 				String line = null;
 				
 				while ((line = reader.readLine()) != null) {
-					OBX obx = constructor.AddSegmentOBX(conceptName, numberOfOBXSegments.toString(), null, null, line, null,
-					    dateTime, hl7Abbreviation, orderRep, numberOfOBXSegments);
-					obx.getObservationResultStatus().setValue(resultStatusValue);
+					addSegmentOBX(conceptName, line, hl7Abbreviation,
+					    numberOfOBXSegments, mdm);
+					
 					numberOfOBXSegments++;
 				}
 				
@@ -149,7 +154,7 @@ public class ExportPowerNote implements ProcessStateAction {
 				sendHL7 = true;
 			
 			if (sendHL7) {
-				return constructor.getMessage();
+				return getMessage(mdm);
 			}
 		}
 		catch (Exception e) {
@@ -159,4 +164,129 @@ public class ExportPowerNote implements ProcessStateAction {
 		return null;
 	}
 	
+	private MSH addSegmentMSH(Encounter enc, MDM_T02 mdm) {
+		
+		MSH msh = mdm.getMSH();
+		String ourApplication = "";
+		String ourFacility = "";
+		String messageType = "";
+		String triggerEvent = "";
+		String version = "";
+		String receivingApp = "";
+		String receivingFacility = "";
+		String ackType = "";
+		String app_acknowledgement_type = "";
+		String processing_id = "";
+		
+		// Get current date
+		String dateFormat = "yyyyMMddHHmmss";
+		SimpleDateFormat formatter = new SimpleDateFormat(dateFormat);
+		String formattedDate = formatter.format(new Date());
+		
+		try {
+			msh.getFieldSeparator().setValue("|");
+			msh.getEncodingCharacters().setValue("^~\\&");
+			msh.getDateTimeOfMessage().getTime().setValue(formattedDate);
+			
+			msh.getSendingApplication().getNamespaceID().setValue(ourApplication);
+			msh.getSendingFacility().getNamespaceID().setValue(ourFacility);
+			msh.getMessageType().getMessageCode().setValue(messageType);
+			msh.getMessageType().getTriggerEvent().setValue(triggerEvent);
+			msh.getMessageControlID().setValue("");
+			msh.getVersionID().getVersionID().setValue(version);
+			msh.getReceivingApplication().getNamespaceID().setValue(receivingApp);
+			msh.getReceivingFacility().getNamespaceID().setValue(receivingFacility);
+			msh.getAcceptAcknowledgmentType().setValue(ackType);
+			msh.getApplicationAcknowledgmentType().setValue(app_acknowledgement_type);
+			msh.getProcessingID().getProcessingID().setValue(processing_id);
+			msh.getMessageControlID().setValue(ourApplication + "-" + formattedDate);
+		}
+		catch (Exception e) {
+			log.error("Exception contructing export message MSH segment. EncounterId: " + enc.getEncounterId(), e);
+		}
+		
+		return msh;
+	}
+	
+	public PID addSegmentPID(Patient pat, MDM_T02 mdm) {
+		
+		PID pid = mdm.getPID();
+		SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
+		Date dob = pat.getBirthdate();
+		String dobStr = "";
+		if (dob != null)
+			dobStr = df.format(dob);
+		
+		try {
+			// Name
+			pid.getPatientName(0).getFamilyName().getSurname().setValue(pat.getFamilyName());
+			pid.getPatientName(0).getGivenName().setValue(pat.getGivenName());
+			
+			// Identifiers
+			PatientIdentifier pi = pat.getPatientIdentifier();
+			
+			// Identifier PID-3
+			// MRN
+			if (pi != null) {
+				String identString = pi.getIdentifier();
+				if (identString != null) {
+					Integer dash = identString.indexOf("-");
+					if (dash >= 0) {
+						identString = identString.substring(0, dash) + identString.substring(dash + 1);
+					}
+				}
+				pid.getPatientIdentifierList(0).getIDNumber().setValue(identString);
+			}
+			
+			// gender
+			pid.getAdministrativeSex().setValue(pat.getGender());
+			
+			// dob
+			pid.getDateTimeOfBirth().getTime().setValue(dobStr);
+			pid.getSetIDPID().setValue("1");
+			
+			return pid;
+			
+		}
+		catch (Exception e) {
+			log.error("Exception adding PID segment to hl7.  PatientId: " + pat.getPatientId(), e);
+			return null;
+		}
+	}
+	
+	public String getMessage(MDM_T02 mdm) {
+		PipeParser pipeParser = new PipeParser();
+		String msg = null;
+		try {
+			msg = pipeParser.encode(mdm);
+		}
+		catch (Exception e) {
+			log.error("Exception parsing constructed message.", e);
+		}
+		return msg;
+		
+	}
+	
+	public OBX addSegmentOBX(String name, String value,String hl7Abbreviation, int obsRep,
+	                         MDM_T02 mdm) {
+		OBX obx = null;
+		
+		try {
+			obx = mdm.getOBXNTE(obsRep).getOBX();
+			obx.getSetIDOBX().setValue(String.valueOf(obsRep + 1));
+			obx.getValueType().setValue(hl7Abbreviation);
+			obx.getObservationIdentifier().getText().setValue(name);
+			obx.getObservationResultStatus().setValue("F");
+			
+			TX tx = new TX(mdm);
+			tx.setValue(value);
+			obx.getObservationValue(0).setData(tx);
+			
+		}
+		catch (Exception e) {
+			log.error("Exception constructing OBX segment for concept ." + name, e);
+		}
+		return obx;
+		
+	}
 }
