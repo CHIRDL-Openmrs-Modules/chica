d3726559d273f89a30bb33f8a71da30101c543e1
sjmckee <sjmckee@iu.edu>
2015-09-02 10:50:23 -0400

    Fixed issues with message type.

--- src/org/openmrs/module/chica/hl7/CurrentDateHL7Filter.java
+++ src/org/openmrs/module/chica/hl7/CurrentDateHL7Filter.java
@@ -10,17 +10,36 @@ import org.apache.commons.logging.LogFactory;
 import org.openmrs.module.sockethl7listener.HL7EncounterHandler;
 import org.openmrs.module.sockethl7listener.HL7Filter;
 
+import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.parser.PipeParser;
+import ca.uhn.hl7v2.util.Terser;
+import ca.uhn.hl7v2.validation.impl.NoValidation;
 
 /**
  * @author tmdugan
  */
 public class CurrentDateHL7Filter implements HL7Filter {
 	
+	private static final String HL7_MESSAGE_TYPE_A04 = "A04";
+	private static final String HL7_MESSAGE_TYPE_A08 = "A08";
+	
 	protected final Log log = LogFactory.getLog(getClass());
 	
 	public boolean ignoreMessage(HL7EncounterHandler hl7EncounterHandler, Message message, String incomingMessageString) {
 		
+		String messageType = null;
+		//get msh-9-2 (message type)
+		PipeParser pipeParser = new PipeParser();
+		pipeParser.setValidationContext(new NoValidation());
+		Terser terser = new Terser(message);
+		try {
+	        messageType = terser.get("/.MSH-9-2");
+        }
+        catch (HL7Exception e) {
+	        log.error("HL7 error parsing message", e);
+        }
+
 		if (hl7EncounterHandler instanceof org.openmrs.module.chica.hl7.mckesson.HL7EncounterHandler25) {
 			
 			Date appointmentTime = ((org.openmrs.module.chica.hl7.mckesson.HL7EncounterHandler25) hl7EncounterHandler)
@@ -28,7 +47,7 @@ public class CurrentDateHL7Filter implements HL7Filter {
 			Date todaysDate = new Date();
 			
 			//process A04 messages
-			if (message instanceof ca.uhn.hl7v2.model.v22.message.ADT_A04) {
+			if (HL7_MESSAGE_TYPE_A04.equals(messageType)) {
 				
 				//if there is no appointment time, process the A04
 				//it is a walkin
@@ -48,7 +67,7 @@ public class CurrentDateHL7Filter implements HL7Filter {
 			//Only process A08's with a non-null appointment time
 			//and an appointment time that is today
 			//This will ignore updates on different days
-			if (message instanceof ca.uhn.hl7v2.model.v22.message.ADT_A08) {
+			if (HL7_MESSAGE_TYPE_A08.equals(messageType)) {
 				if (appointmentTime!=null&&appointmentTime.getDate() == todaysDate.getDate() && appointmentTime.getMonth() == todaysDate.getMonth()
 				        && appointmentTime.getYear() == todaysDate.getYear()) {
 					return false;
