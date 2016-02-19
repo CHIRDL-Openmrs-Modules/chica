dd70af465f98beb07e96364dd04397dffe6aa4b4
msheley <msheley@iu.edu>
2016-02-19 12:23:52 -0500

    Add sleep and retry if chirp not available, add logging, and remove checking for no hpv history.  Project 20 has enrollees with no hpv history.

--- src/org/openmrs/module/chica/BatchImmunizationQuery.java
+++ src/org/openmrs/module/chica/BatchImmunizationQuery.java
@@ -55,23 +55,30 @@ public class BatchImmunizationQuery extends AbstractTask {
 	private static final String PROPERTY_KEY_FOLLOWUP_CONCEPT = "followup_concept";
 	private static final String PROPERTY_KEY_MAX_NUMBER_OF_ENCOUNTERS = "max_number_of_encounters";
 	private static final String PROPERTY_KEY_SLEEP_TIME_MS = "sleep_time_msec";
+	private static final String PROPERTY_KEY_RETRY_SLEEP_TIME_MIN = "retry_sleep_time_minutes";
 	private static final String CHIRP_STATUS_CONCEPT = "CHIRP_Status";
 	private static final String HPV_VACCINE_NAME = "HPV, unspecified formulation";
+	private static final String PROPERTY_KEY_MAX_RETRY = "max_retry_count";
 	
 	private Log log = LogFactory.getLog(this.getClass());
 
 	private TaskDefinition taskConfig;
 	private String enrollmentConceptProperty; //String containing concept name
 	private String maxEncounterCountProperty;
+	private String maxRetryCountProperty;
 	private String startDateProperty;
 	private String stopDateProperty;
 	private String followupConceptProperty;
 	private Concept chirpStatusConcept;
 	private String sleepTimeProperty;
+	private String retrySleepTimeProperty;
 	private Integer maxEncounterCount = 0;
 	private Date startDate = null;
 	private Date stopDate = null;
 	private Integer sleep = null;
+	private Integer retrySleep = null;
+	private Integer maxRetries = 0;
+	
 	
 	
 	@Override
@@ -88,7 +95,9 @@ public class BatchImmunizationQuery extends AbstractTask {
 		    stopDateProperty = this.taskConfig.getProperty(PROPERTY_STOP_DATE);
 		    followupConceptProperty = this.taskConfig.getProperty(PROPERTY_KEY_FOLLOWUP_CONCEPT);
 		    maxEncounterCountProperty = this.taskConfig.getProperty(PROPERTY_KEY_MAX_NUMBER_OF_ENCOUNTERS);
+		    maxRetryCountProperty = this.taskConfig.getProperty(PROPERTY_KEY_MAX_RETRY);
 		    sleepTimeProperty = this.taskConfig.getProperty(PROPERTY_KEY_SLEEP_TIME_MS);
+		    retrySleepTimeProperty = this.taskConfig.getProperty(PROPERTY_KEY_RETRY_SLEEP_TIME_MIN);
 		    //start and stop dates are not required
 		    //StringUtils isBlank() and isNumeric() have null checking
 		    if (StringUtils.isBlank(enrollmentConceptProperty)) {
@@ -103,8 +112,13 @@ public class BatchImmunizationQuery extends AbstractTask {
 				log.error("Batch immunization query task property '" + PROPERTY_KEY_MAX_NUMBER_OF_ENCOUNTERS + "' is not present in the property list for this task");
 				return;
 			}
+		    
 		    if (StringUtils.isNumeric(sleepTimeProperty) && !StringUtils.isWhitespace(sleepTimeProperty)) {
 				sleep = Integer.valueOf(sleepTimeProperty);
+			}
+		    if (StringUtils.isNumeric(retrySleepTimeProperty) && !StringUtils.isWhitespace(retrySleepTimeProperty)) {
+		    	retrySleep = Integer.valueOf(retrySleepTimeProperty);
+		    	retrySleep = retrySleep * 60000;
 			}
 		    
 		    try {
@@ -138,29 +152,35 @@ public class BatchImmunizationQuery extends AbstractTask {
 
 			Concept enrollmentConcept = conceptService.getConceptByName(enrollmentConceptProperty);
 			if (enrollmentConcept == null ) {
-				log.error ("HPV study:  Task property '" + PROPERTY_KEY_ENROLLMENT_CONCEPT + "' is not a valid concept");
+				log.info ("HPV study:  Task property '" + PROPERTY_KEY_ENROLLMENT_CONCEPT + "' is not a valid concept");
 				return;
 			}
 			Concept followUpConcept = conceptService.getConceptByName(followupConceptProperty);
 			if (followUpConcept == null) {
-				log.error ("HPV study:  Task property '" + PROPERTY_KEY_FOLLOWUP_CONCEPT + "' is not a valid concept");
+				log.info ("HPV study:  Task property '" + PROPERTY_KEY_FOLLOWUP_CONCEPT + "' is not a valid concept");
 				return;
 			}
 			
 			try {
 				maxEncounterCount = Integer.valueOf(maxEncounterCountProperty.trim());
 			} catch (NumberFormatException e) {
-				log.error("HPV Study: Task property 'max_number_of_encounters' could not be parsed as an Integer");
+				log.info("HPV Study: Task property 'max_number_of_encounters' could not be parsed as an Integer");
 				return;
+			}
+			
+			try {
+				maxRetries = Integer.valueOf(maxRetryCountProperty.trim());
+			} catch (NumberFormatException e) {
+				log.info("HPV Study: Task property 'max_retry_count' could not be parsed as an Integer");
 			}
 			
 			chirpStatusConcept = conceptService.getConceptByName(CHIRP_STATUS_CONCEPT);
 			if (chirpStatusConcept == null){
-				log.error("HPV study: '"+ CHIRP_STATUS_CONCEPT + "'is not a valid concept.");
+				log.info("HPV study: '"+ CHIRP_STATUS_CONCEPT + "'is not a valid concept.");
 				return;
 			}
 			if (chirpStatusConcept == null){
-				log.error("HPV study: '"+ CHIRP_STATUS_CONCEPT + "'is not a valid concept.");
+				log.info("HPV study: '"+ CHIRP_STATUS_CONCEPT + "'is not a valid concept.");
 				return;
 			}
 			
@@ -180,8 +200,10 @@ public class BatchImmunizationQuery extends AbstractTask {
 		ObsService obsService = Context.getObsService();
 		//Count the queries to CHIRP. This is not updated unless a query was performed.
 		int numberOfQueries = 0;
-		int numberOfFailedQueries = 0;
+		int failureCount = 0;
 		int errorCount  = 0;
+		int retries = 0;
+		
 		String identifier = "";
 		
 
@@ -202,61 +224,63 @@ public class BatchImmunizationQuery extends AbstractTask {
 			Encounter encounter = encountersIterator.next();
 			
 			try {
-
+				Integer vaccineCount = 0;
+				retries = 0;
+				Date startQuery = new Date();
 				String queryResponse = ImmunizationRegistryQuery.queryCHIRP(encounter);
-				Integer count = 0;
-				/* Check the latest CHIRP status observation today for this requery.  
-				 * We do not want to send any more immunization queries to CHIRP
-				 * if CHIRP is not available.  
+
+				/* Queries can fail due to CHIRP access, invalid CHIRP response, CHIRP could not find patient,
+				 * parsing error, or patient not match to our database.
 				 */
-
-				if (queryResponse == null) {
-
-					Date now = new Date();
-					Integer mostRecentCount = 1;
-					List<Obs> latestChirpStatusObs = obsService.getObservations(
-							Collections.singletonList((Person)encounter.getPatient()), 
-							Collections.singletonList((org.openmrs.Encounter) encounter), 
-							Collections.singletonList(chirpStatusConcept), 
-							null, null, null, null, mostRecentCount, null, DateUtil.getStartOfDay(now), now, false);
+				//If it is a chirp availablility issue, sleep and retry the query
+				while (queryResponse == null 
+						&& ( isChirpIssue(encounter, startQuery)) 
+						&& retries <= maxRetries){
 					
-					Iterator<Obs> iter = latestChirpStatusObs.iterator();
-					if (iter.hasNext()){
-						Obs chirpStatusObs = iter.next();
-						if (CHIRP_NOT_AVAILABLE.equals(chirpStatusObs.getValueAsString(Locale.US))||
-								CHIRP_LOGIN_FAILED.equals(chirpStatusObs.getValueAsString(Locale.US))){
-							log.info("HPV Study: Follow-up CHIRP query problems due to CHIRP availability. \r\n"
-									+ "Number of encounters = " + numberOfEncounters + ".\r\n"
-									+ "Number of CHIRP queries performed before CHIRP error = " + numberOfQueries + ".\r\n"
-									+ "Number of failed queries prior to CHIRP error = " + numberOfFailedQueries + ".\r\n"
-									+ "Error count prior to CHIRP error = " + errorCount);
-							return;
-						}
+					log.info("CHIRP not available, retry query in " + (retrySleep == null||retrySleep == 0 ? 0 :retrySleep/60000) + " min");
+					if (retrySleep != null) {
+						Thread.sleep(retrySleep);
 					}
-					//Queries can fail due to CHIRP access, invalid CHIRP response, CHIRP could not find patient,
-					//parsing error, or patient not match to our database.
-					numberOfFailedQueries++;
+					startQuery = new Date();
+					queryResponse = ImmunizationRegistryQuery.queryCHIRP(encounter);
+					retries++;
+				}
+				//Last retry was still null
+				if (queryResponse == null){
+					failureCount++;
+					//Check if it is still a CHIRP issue but ran out of retries
+					if (isChirpIssue(encounter, startQuery)){
+						//Chirp problems, but reached max retry limit
+						log.info("CHIRP query problems due to CHIRP availability. PatientId = " + encounter.getPatientId() + ".\r\n" 
+								+ "Number of encounters = " + numberOfEncounters + ".\r\n"
+								+ "Number of CHIRP queries performed before CHIRP error = " + numberOfQueries + ".\r\n"
+								+ "Number of failed queries = " + failureCount  + ".\r\n"
+								+ "Number of retries = " + retries + ".\r\n");
+						return;
+					}
+					//Not a chirp issue, but possible problems such as patient matching and parsing errors
 					continue;
 				}
+
 
 				ImmunizationQueryOutput immunizations = ImmunizationForecastLookup
 						.getImmunizationList(encounter.getPatientId());
 
 				identifier = encounter.getPatient().getPatientIdentifier().toString();
-				
+
 				if (immunizations != null) {
 					HashMap<String, HashMap<Integer, ImmunizationPrevious>> prevImmunizations = immunizations
 							.getImmunizationPrevious();
-	
+
 					if (prevImmunizations != null){
-	
+
 						HashMap<Integer, ImmunizationPrevious> HpvHistory = prevImmunizations.get(HPV_VACCINE_NAME);
-						
+
 						if (HpvHistory != null){
-						
+							
 							for(ImmunizationPrevious value : HpvHistory.values()){
 								if (value.getDate().before(DateUtil.getStartOfDay(encounter.getEncounterDatetime()))){
-									count++;
+									vaccineCount++;
 								}
 							}
 						}
@@ -266,14 +290,14 @@ public class BatchImmunizationQuery extends AbstractTask {
 				Patient patient = new Patient();
 				patient = encounter.getPatient();
 				Obs obs = new Obs();
-				obs.setValueNumeric(count.doubleValue());
+				obs.setValueNumeric(vaccineCount.doubleValue());
 				obs.setEncounter(encounter);
 				obs.setPerson(patient);
 				obs.setConcept(followUpConcept);
 				obs.setObsDatetime(encounter.getEncounterDatetime());
 				obsService.saveObs(obs, null);
 				if (sleep != null) Thread.sleep(sleep);
-		
+
 			}catch (InterruptedException e){
 				log.info("Exception executing Thread.sleep. Sleep time = " +  sleep);
 			}catch(Exception e){
@@ -288,10 +312,41 @@ public class BatchImmunizationQuery extends AbstractTask {
 		
 		log.info("Batch immunization query completed. \r\n Number of encounters = " + numberOfEncounters + ".\r\n"
 				+ "Number of CHIRP queries performed = " + numberOfQueries + ".\r\n"
-				+ "Number of failed queries = " + numberOfFailedQueries + ".\r\n"
+				+ "Number of failed queries = " + failureCount + ".\r\n"
 				+ "Error count = " + errorCount);
 		
 		return;
+	}
+	
+	/**
+	 * Check if the latest CHIRP status observation indicates CHIRP is not available.  
+	 * @param encounter
+	 * @param startQuery
+	 * @return
+	 */
+	private boolean isChirpIssue(Encounter encounter, Date startQuery){
+		 
+		ObsService obsService = Context.getObsService();
+		Date now = new Date();
+		Integer mostRecentCount = 1;
+		boolean isChirp = false;
+		List<Obs> latestChirpStatusObs = obsService.getObservations(
+				Collections.singletonList((Person)encounter.getPatient()), 
+				Collections.singletonList((org.openmrs.Encounter) encounter), 
+				Collections.singletonList(chirpStatusConcept), 
+				null, null, null, null, mostRecentCount, null, startQuery, now, false);
+		
+		Iterator<Obs> iter = latestChirpStatusObs.iterator();
+		if (iter.hasNext()){
+			Obs chirpStatusObs = iter.next();
+			if (CHIRP_NOT_AVAILABLE.equals(chirpStatusObs.getValueAsString(Locale.US))||
+					CHIRP_LOGIN_FAILED.equals(chirpStatusObs.getValueAsString(Locale.US))){
+				isChirp = true;
+			}
+			
+		}
+	
+		return isChirp;
 	}
 
 	@Override
