d82c0cc0066f79858f2b6778624fe64d05d0f37f
seemasarala <ssarala@IN-PEDS-00426.ads.iu.edu>
2016-05-05 11:05:01 -0400

    CHICA-576: Fixed error message in the GreaseBoard form while entering a patient who is not checked in

--- web/module/resources/greaseBoard.js
+++ web/module/resources/greaseBoard.js
@@ -494,7 +494,6 @@ function checkPrintHandoutsMRN() {
 	      "url": url,
 	      "timeout": 60000, // optional if you want to handle timeouts (which you should)
 	      "error": handleVerifyPrintHandoutsMRNAjaxError, // this sets up jQuery to give me errors
-	      "success": function (xml) {
 	          verifyPrintHandoutsMRN(xml);
 	      }
 	  });
@@ -762,7 +761,8 @@ function verifyPrintHandoutsMRN(responseXML) {
         $("#printHandoutsMrnError").show("highlight", 750);
     } else {
     	var result = $(responseXML).find("result").text();
-        if (result == "true") {
+		var sessionResult = $(responseXML).find("sessionResult").text();
+		if (result == "true" && sessionResult == "true") {
         	$("#printHandoutsMrnError").hide();
         	$("#patientId").val("");
         	$("#sessionId").val("");
@@ -776,8 +776,11 @@ function verifyPrintHandoutsMRN(responseXML) {
         	$("#printHandoutsMRNDialog").dialog("close");
         	$("#printHandoutsMRNDialog").dialog("option", "hide", { effect: "fade", duration: 500 } );
         	$("#force-print-dialog").dialog("open");
-        } else {
+        } else if (result == "false"){
         	$("#printHandoutsMrnMessage").html("<p><b>MRN is not valid.<br>Retype the MRN #. Press OK to display the patient handouts.</b></p>");
+            $("#printHandoutsMrnError").show("highlight", 750);
+        } else if (sessionResult == "false"){
+        	$("#printHandoutsMrnMessage").html("<p><b>Patient is not checked in the system.<br>Retype the MRN #. Press OK to display the patient handouts.</b></p>");
             $("#printHandoutsMrnError").show("highlight", 750);
         }
     }
