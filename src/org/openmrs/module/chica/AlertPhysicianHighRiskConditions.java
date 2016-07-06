1c0dbb8f0d84d9981bf946ca66db8007d8af6c36
tmdugan <tmdugan@iu.edu>
2016-07-06 09:55:24 -0400

    Split up abuse and suicide

--- src/org/openmrs/module/chica/AlertPhysicianHighRiskConditions.java
+++ src/org/openmrs/module/chica/AlertPhysicianHighRiskConditions.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +15,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Concept;
 import org.openmrs.Encounter;
+import org.openmrs.Form;
 import org.openmrs.Location;
 import org.openmrs.Obs;
 import org.openmrs.Person;
@@ -23,6 +25,7 @@ import org.openmrs.api.ObsService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.chica.util.Util;
 import org.openmrs.module.chirdlutilbackports.hibernateBeans.PatientState;
+import org.openmrs.module.chirdlutilbackports.hibernateBeans.State;
 import org.openmrs.module.chirdlutilbackports.service.ChirdlUtilBackportsService;
 import org.openmrs.scheduler.tasks.AbstractTask;
 import org.openmrs.util.OpenmrsConstants.PERSON_TYPE;
@@ -40,7 +43,6 @@ public class AlertPhysicianHighRiskConditions extends AbstractTask {
 	@Override
 	public void execute() {
 		Context.openSession();
-		ChirdlUtilBackportsService backportsService = Context.getService(ChirdlUtilBackportsService.class);
 		
 		Calendar date = Calendar.getInstance();
 		date.set(Calendar.HOUR_OF_DAY, 0);
@@ -55,26 +57,52 @@ public class AlertPhysicianHighRiskConditions extends AbstractTask {
 		Date endDate = date.getTime();
 		
 		EncounterService encounterService = Context.getEncounterService();
+		
+		//get encounters that should have been submitted but have not been processed by the task
 		List<Encounter> encounters = encounterService.getEncounters(null, null, startDate, endDate, null, null, null, false);
 		ObsService obsService = Context.getObsService();
-		List<Concept> questions = new ArrayList<Concept>();
 		ConceptService conceptService = Context.getConceptService();
+		HashSet<Encounter> notificationSet = new HashSet<Encounter>();
+		
+		//get suicide observations
+		List<Concept> questions = new ArrayList<Concept>();
 		questions.add(conceptService.getConceptByName("suicide_concerns"));
-		questions.add(conceptService.getConceptByName("Abuse_Concern"));
 		List<Concept> answers = new ArrayList<Concept>();
 		answers.add(conceptService.getConceptByName("True"));
-		answers.add(conceptService.getConceptByName("yes_parent"));
 		
 		List<Obs> obs = obsService.getObservations(null, encounters, questions, answers, null, null, null, null, null, null,
 		    null, false);
+		
+		addEncounters(obs, notificationSet);
+		
+		//get abuse observations
+		questions = new ArrayList<Concept>();
+		questions.add(conceptService.getConceptByName("Abuse_Concern"));
+		answers = new ArrayList<Concept>();
+		answers.add(conceptService.getConceptByName("yes_parent"));
+		
+		obs = obsService.getObservations(null, encounters, questions, answers, null, null, null, null, null, null, null,
+		    false);
+		
+		addEncounters(obs, notificationSet);
+		
+		//TODO construct and send email
+		
+		Context.closeSession();
+		
+	}
+	
+	private static void addEncounters(List<Obs> obs, HashSet<Encounter> notificationSet) {
+		ChirdlUtilBackportsService backportsService = Context.getService(ChirdlUtilBackportsService.class);
 		
 		for (Obs currOb : obs) {
 			Encounter encounter = currOb.getEncounter();
 			Integer encounterId = encounter.getEncounterId();
 			Map<Integer, List<PatientState>> formIdToPatientStateMapEnd = new HashMap<Integer, List<PatientState>>();
-			
-			Integer formId = null; //TODO set this value
-			Integer endStateId = null; //TODO set this value
+			Form form = Context.getFormService().getForm("PWS");
+			Integer formId = form.getFormId();
+			State endState = backportsService.getStateByName("PWS_process");
+			Integer endStateId = endState.getStateId();
 			Util.getPatientStatesByEncounterId(backportsService, formIdToPatientStateMapEnd, encounterId, endStateId, true);
 			
 			boolean containsEndState = formIdToPatientStateMapEnd.containsKey(formId);
@@ -83,11 +111,10 @@ public class AlertPhysicianHighRiskConditions extends AbstractTask {
 				//if the PWS was submitted, see if any checkboxes on prompt
 			} else {
 				//if the PWS was not submitted, add the encounter to the notification list
+				notificationSet.add(encounter);
 			}
 			
 		}
-		
-		Context.closeSession();
 		
 	}
 	
