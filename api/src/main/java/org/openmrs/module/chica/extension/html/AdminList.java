7d7d3182c189bef148ee3f57afea22420bfb9642
Dave <davely@iu.edu>
2017-05-24 15:03:00 -0400

    CHICA-987/988 Remove scan PSF/PWS test pages under Admin section

--- api/src/main/java/org/openmrs/module/chica/extension/html/AdminList.java
+++ api/src/main/java/org/openmrs/module/chica/extension/html/AdminList.java
@@ -29,8 +29,6 @@ public class AdminList extends AdministrationSectionExt {
 		
 		map.put("module/chica/testCheckin.form", "Test checkin through AOP");
 		map.put("module/chica/parseDictionary.form", "Parse dictionary file");
-		map.put("module/chica/fillOutPSF.form?formName=PSF", "Scan PSF");
-		map.put("module/chica/fillOutPWS.form?formName=PWS", "Scan PWS");
 		map.put("module/chica/greaseBoard.form", "Grease Board");
 		map.put("module/chica/viewPatient.form", "View Encounters");
 		map.put("module/chica/chicaRuleTester.form", "Rule Tester");
