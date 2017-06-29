eafba33182bc8e63fb9f0762099f9d346ab35be5
ssarala <ssarala@IN-PEDS-00426.ads.iu.edu>
2017-06-29 15:03:38 -0400

    Updated sqldiff

--- api/src/main/java/org/openmrs/module/chica/action/GenericAction.java
+++ api/src/main/java/org/openmrs/module/chica/action/GenericAction.java
@@ -3,14 +3,12 @@ package org.openmrs.module.chica.action;
 import java.util.HashMap;
 
 import org.openmrs.Patient;
-import org.openmrs.api.context.Context;
 import org.openmrs.module.chirdlutilbackports.BaseStateActionHandler;
 import org.openmrs.module.chirdlutilbackports.StateManager;
 import org.openmrs.module.chirdlutilbackports.action.ProcessStateAction;
 import org.openmrs.module.chirdlutilbackports.hibernateBeans.PatientState;
 import org.openmrs.module.chirdlutilbackports.hibernateBeans.State;
 import org.openmrs.module.chirdlutilbackports.hibernateBeans.StateAction;
-import org.openmrs.module.chirdlutilbackports.service.ChirdlUtilBackportsService;
 
 /**
  * Temporary placeholder for GenericAction action
@@ -31,9 +29,7 @@ public class GenericAction implements ProcessStateAction
 	@Override
 	public void processAction(StateAction stateAction, Patient patient, PatientState patientState, HashMap<String, Object> parameters) 
 	{
-		ChirdlUtilBackportsService chirdlutilbackportsService = Context.getService(ChirdlUtilBackportsService.class);
 		Integer sessionId = patientState.getSessionId();
-		Integer encounterId = chirdlutilbackportsService.getSession(sessionId).getEncounterId();
 		State currState = patientState.getState();
 		Integer locationTagId = patientState.getLocationTagId();
 		Integer locationId = patientState.getLocationId();
--- api/src/main/java/org/openmrs/module/chica/action/QueryCDA.java
+++ api/src/main/java/org/openmrs/module/chica/action/QueryCDA.java
@@ -1,20 +1,14 @@
 package org.openmrs.module.chica.action;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import org.openmrs.Patient;
-import org.openmrs.api.context.Context;
-import org.openmrs.logic.result.Result;
 import org.openmrs.module.chirdlutilbackports.BaseStateActionHandler;
 import org.openmrs.module.chirdlutilbackports.StateManager;
 import org.openmrs.module.chirdlutilbackports.action.ProcessStateAction;
 import org.openmrs.module.chirdlutilbackports.hibernateBeans.PatientState;
 import org.openmrs.module.chirdlutilbackports.hibernateBeans.State;
 import org.openmrs.module.chirdlutilbackports.hibernateBeans.StateAction;
-import org.openmrs.module.chirdlutilbackports.service.ChirdlUtilBackportsService;
-import org.openmrs.module.dss.hibernateBeans.Rule;
-import org.openmrs.module.dss.service.DssService;
 
 /**
  * Temporary placeholder for QueryCDA action
@@ -35,9 +29,7 @@ public class QueryCDA implements ProcessStateAction
 	@Override
 	public void processAction(StateAction stateAction, Patient patient, PatientState patientState, HashMap<String, Object> parameters) 
 	{
-		ChirdlUtilBackportsService chirdlutilbackportsService = Context.getService(ChirdlUtilBackportsService.class);
 		Integer sessionId = patientState.getSessionId();
-		Integer encounterId = chirdlutilbackportsService.getSession(sessionId).getEncounterId();
 		State currState = patientState.getState();
 		Integer locationTagId = patientState.getLocationTagId();
 		Integer locationId = patientState.getLocationId();
