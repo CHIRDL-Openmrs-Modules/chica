6ba9daa64ceab744b5fdbf6de40044432653188e
Steve McKee <sjmckee@iu.edu>
2018-02-20 11:34:12 -0500

    CHICA-1192: Fix view encounter slowness.

--- omod/src/main/java/org/openmrs/module/chica/web/ManualCheckinSSNMRN.java
+++ omod/src/main/java/org/openmrs/module/chica/web/ManualCheckinSSNMRN.java
@@ -10,13 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Encounter;
-import org.openmrs.PatientIdentifierType;
+import org.openmrs.Patient;
 import org.openmrs.api.EncounterService;
-import org.openmrs.api.PatientIdentifierException;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.chirdlutil.util.ChirdlUtilConstants;
 import org.openmrs.module.chirdlutil.util.Util;
-import org.openmrs.validator.PatientIdentifierValidator;
+import org.openmrs.parameter.EncounterSearchCriteriaBuilder;
 
 public class ManualCheckinSSNMRN {
 	
@@ -39,38 +38,28 @@ public class ManualCheckinSSNMRN {
 		PrintWriter pw = response.getWriter();
 		pw.write(XML_MRN_VERIFICATION_START);
 		String mrn = request.getParameter(PARAM_MRN);
-		mrn = Util.removeLeadingZeros(mrn);
-		
-		boolean valid = false;
-		if (mrn != null && !mrn.contains("-") && mrn.length() > 1) {
-			mrn = mrn.substring(0, mrn.length() - 1) + "-" + mrn.substring(mrn.length() - 1);
+		Patient patient = Util.getPatientByMRNOther(mrn);
+		boolean validPatient = false;
+		List<Encounter> encounters = null;
+		if (patient != null) {
+			validPatient = true;
 		}
 		
-		try {
-			if (mrn != null && mrn.length() > 0 && !mrn.endsWith("-")) {
-				PatientIdentifierType mrnIdent = 
-						Context.getPatientService().getPatientIdentifierTypeByName(ChirdlUtilConstants.IDENTIFIER_TYPE_MRN);
-				try {
-					PatientIdentifierValidator.validateIdentifier(mrn, mrnIdent);
-					valid = true;
-				}  catch(PatientIdentifierException e) {
-				}
-			}
+		if (validPatient) {
+			EncounterService encounterService = Context.getEncounterService();
+			EncounterSearchCriteriaBuilder criteriaBuilder = new EncounterSearchCriteriaBuilder();
+			criteriaBuilder.setPatient(patient);
+			criteriaBuilder.setIncludeVoided(false);
+			encounters = encounterService.getEncounters(criteriaBuilder.createEncounterSearchCriteria());
 		}
-		catch (Exception e) {
-			log.error("Error validating MRN: " + mrn);
-		}
-		
-		EncounterService encounterService = Context.getEncounterService();
-		List<Encounter> encounters = encounterService.getEncountersByPatientIdentifier(mrn);
 						
-		if (valid) {
+		if (validPatient) {
 			pw.write(XML_RESULT_START + XML_TRUE + XML_RESULT_END);
 		} else {
 			pw.write(XML_RESULT_START + XML_FALSE + XML_RESULT_END);
 		}
 		
-		if (encounters.size() > 0) {
+		if (validPatient && encounters.size() > 0) {
 			pw.write(XML_VALID_ENCOUNTER_START + XML_TRUE + XML_VALID_ENCOUNTER_END);
 		}else{
 			pw.write(XML_VALID_ENCOUNTER_START + XML_FALSE + XML_VALID_ENCOUNTER_END);
