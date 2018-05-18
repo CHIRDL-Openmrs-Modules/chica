c500b82f5445385719199388b974faf93b067b1e
Steve McKee <sjmckee@iu.edu>
2018-05-18 14:13:50 -0400

    CHICA-1248: Added back 15 timer to submit page.

--- omod/src/main/webapp/resources/finishFormsMobile.js
+++ omod/src/main/webapp/resources/finishFormsMobile.js
@@ -1,3 +1,7 @@
+$(document).on("pagebeforeshow", "#finished_form", function() {
+    startSubmitTimer();
+});
+
 $(document).on("pageshow", "#finished_form", function(){
 	if ($("#userQuitForm").val() !== "true") {
 	    $("#finished_dialog").popup("open", { 
@@ -6,11 +10,26 @@ $(document).on("pageshow", "#finished_form", function(){
 	}
 });
 
+/**
+ * Timer to automatically submit the page after 15 minutes of inactivity.
+ */
+function startSubmitTimer() {
+    var timer = $.timer(function () {
+    	finish();
+    });
+
+    timer.set({
+        time: 900000,
+        autostart: true
+    });
+}
+
 function finish() {
     $("#loadingDialog").popup("open", { 
         transition: "pop"
     });
-    document.getElementById('complete_form').submit();
+    
+    document.getElementById("complete_form").submit();
 }
 
 function closeFinishDialog() {
