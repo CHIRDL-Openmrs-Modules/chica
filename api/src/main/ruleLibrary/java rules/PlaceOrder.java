8a831ea9ec0e934fba6c411a2595e3e5c6a21b03
Steve McKee <sjmckee@iu.edu>
2020-12-21 12:05:50 -0500

    CHICA-1438: Initial commit to place an order.

---
+++ api/src/main/ruleLibrary/java rules/PlaceOrder.java
@@ -0,0 +1,166 @@
+package org.openmrs.module.chica.rule;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.CareSetting;
+import org.openmrs.Concept;
+import org.openmrs.Encounter;
+import org.openmrs.Order;
+import org.openmrs.OrderType;
+import org.openmrs.Patient;
+import org.openmrs.api.context.Context;
+import org.openmrs.logic.LogicContext;
+import org.openmrs.logic.Rule;
+import org.openmrs.logic.result.Result;
+import org.openmrs.logic.result.Result.Datatype;
+import org.openmrs.logic.rule.RuleParameterInfo;
+
+/**
+ * Places an order to the orders table.
+ * 
+ * @author Steve McKee
+ */
+public class PlaceOrder implements Rule {
+	
+	private Log log = LogFactory.getLog(this.getClass());
+	
+	/**
+	 * @see org.openmrs.logic.Rule#eval(org.openmrs.logic.LogicContext, java.lang.Integer, java.util.Map)
+	 */
+	@Override
+	public Result eval(LogicContext context, Integer patientId, Map<String, Object> parameters) {
+		if (patientId == null) {
+			this.log.error("Cannot place order.  Patient ID is null.");
+			return Result.emptyResult();
+		}
+		
+		if (parameters == null) {
+			this.log.error("Cannot place order.  Paremters are null.");
+			return Result.emptyResult();
+		}
+		
+		String conceptName = (String)parameters.get("param1");
+		if (StringUtils.isBlank(conceptName)) {
+			this.log.error("Cannot place order.  No order name provided.");
+			return Result.emptyResult();
+		}
+		
+		Integer encounterId = (Integer)parameters.get("encounterId");
+		if (encounterId == null) {
+			this.log.error("Cannot place order.  No encounter ID available.");
+			return Result.emptyResult();
+		}
+		
+		Patient patient = Context.getPatientService().getPatient(patientId);
+		if (patient == null) {
+			this.log.error("Cannot place order.  No patient found for ID " + patientId);
+			return Result.emptyResult();
+		}
+		
+		Encounter encounter = Context.getEncounterService().getEncounter(encounterId);
+		if (encounter == null) {
+			this.log.error("Cannot place order.  No encounter found for ID " + encounterId);
+			return Result.emptyResult();
+		}
+		
+		if (encounter.getActiveEncounterProviders().isEmpty()) {
+			this.log.error("Cannot place order.  No encounter provider found for encounter " + encounterId);
+			return Result.emptyResult();
+		}
+		
+		String orderTypeName = (String)parameters.get("param2");
+		if (StringUtils.isBlank(orderTypeName)) {
+			this.log.error("Cannot place order.  Order type not provided.");
+			return Result.emptyResult();
+		}
+		
+		OrderType orderType = Context.getOrderService().getOrderTypeByName(orderTypeName);
+		if (orderType == null) {
+			this.log.error("Cannot place order.  Order type with the following name cannot be found: " + orderTypeName);
+		}
+		
+		String careSettingName = (String)parameters.get("param3");
+		if (StringUtils.isBlank(careSettingName)) {
+			this.log.error("Cannot place order.  Care setting not provided.");
+			return Result.emptyResult();
+		}
+		
+		CareSetting careSetting = Context.getOrderService().getCareSettingByName(careSettingName);
+		if (careSetting == null) {
+			this.log.error("Cannot place order.  Care setting with the following name cannot be found: " 
+				+ careSettingName);
+			return Result.emptyResult();
+		}
+		
+		return placeOrder(patient, encounter, careSetting, orderType, conceptName);
+	}
+	
+	/**
+	 * @see org.openmrs.logic.Rule#getParameterList()
+	 */
+	@Override
+	public Set<RuleParameterInfo> getParameterList() {
+		return new HashSet<>();
+	}
+	
+	/**
+	 * @see org.openmrs.logic.Rule#getDependencies()
+	 */
+	@Override
+	public String[] getDependencies() {
+		return new String[0];
+	}
+	
+	/**
+	 * @see org.openmrs.logic.Rule#getTTL()
+	 */
+	@Override
+	public int getTTL() {
+		return 0;
+	}
+	
+	/**
+	 * @see org.openmrs.logic.Rule#getDefaultDatatype()
+	 */
+	@Override
+	public Datatype getDefaultDatatype() {
+		return Datatype.CODED;
+	}
+	
+	/**
+	 * Places the order into the OpenMrs orders table.
+	 * 
+	 * @param patient The patient the order is for
+	 * @param encounter The encounter for the order
+	 * @param careSetting The care setting
+	 * @param orderType The order type
+	 * @param orderConceptName The concept name for the order
+	 * @return Result object if the order is successfully placed
+	 */
+	private Result placeOrder(Patient patient, Encounter encounter, CareSetting careSetting, OrderType orderType, 
+			String orderConceptName) {
+		Concept orderConcept = Context.getConceptService().getConceptByName(orderConceptName);
+		if (orderConcept == null) {
+			this.log.error("Cannot place order.  Order concept with the following name cannot be found: "
+				+ orderConceptName);
+			return Result.emptyResult();
+		}
+		
+		Order order = new Order();
+		order.setPatient(patient);
+		order.setEncounter(encounter);
+		order.setConcept(orderConcept);
+		order.setOrderer(encounter.getActiveEncounterProviders().iterator().next().getProvider());
+		order.setAction(Order.Action.NEW);
+		order.setCareSetting(careSetting);
+		order.setOrderType(orderType);
+		
+		Context.getOrderService().saveOrder(order, null);
+		return new Result(orderConcept);
+	}
+}
