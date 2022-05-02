9bbc170bd1f57650c7e3bd9e04e164a488c263a9
Seema Sarala <ssarala@iu.edu>
2022-05-02 08:46:59 -0700

    Committing new files

---
+++ api/src/main/java/org/openmrs/module/chica/hibernateBeans/MDbmiageinf.java
@@ -0,0 +1,324 @@
+package org.openmrs.module.chica.hibernateBeans;
+
+import org.openmrs.module.chica.MDPercentile;
+
+/**
+ * Holds information to store in the chica_md_bmiage table
+ * 
+ * @author Seema Sarala
+ * @version 1.0
+ */
+public class MDbmiageinf implements java.io.Serializable, MDPercentile {
+
+    private static final long serialVersionUID = 1L;
+    // Fields
+    private Integer mdbmiageinfId = null;
+    private Double ageYears = null;
+    private Double meanAge = null;
+    private Double sample = null;
+    private Double mean = null;
+    private Double sd = null;
+    private Double p10 = null;
+    private Double p25 = null;
+    private Double p50 = null;
+    private Double p75 = null;
+    private Double p90 = null;
+    private Double gtBelowMean = null;
+
+    /**
+     * @return the mdbmiageinfId
+     */
+    public Integer getMdbmiageinfId()
+    {
+        return this.mdbmiageinfId;
+    }
+
+    /**
+     * @param mdbmiageinfId the mdbmiageinfId to set
+     */
+    public void setMdbmiageinfId(Integer mdbmiageinfId)
+    {
+        this.mdbmiageinfId = mdbmiageinfId;
+    }
+
+    /**
+     * @return the ageYears
+     */
+    public Double getAgeYears()
+    {
+        return this.ageYears;
+    }
+
+    /**
+     * @param ageYears the ageYears to set
+     */
+    public void setAgeYears(Double ageYears)
+    {
+        this.ageYears = ageYears;
+    }
+
+    /**
+     * @return the meanAge
+     */
+    @Override
+    public Double getMeanAge()
+    {
+        return this.meanAge;
+    }
+
+    /**
+     * @param meanAge the meanAge to set
+     */
+    public void setMeanAge(Double meanAge)
+    {
+        this.meanAge = meanAge;
+    }
+
+    /**
+     * @return the sample
+     */
+    @Override
+    public Double getSample()
+    {
+        return this.sample;
+    }
+
+    /**
+     * @param sample the sample to set
+     */
+    public void setSample(Double sample)
+    {
+        this.sample = sample;
+    }
+
+    /**
+     * @return the mean
+     */
+    @Override
+    public Double getMean()
+    {
+        return this.mean;
+    }
+
+    /**
+     * @param mean the mean to set
+     */
+    public void setMean(Double mean)
+    {
+        this.mean = mean;
+    }
+
+    /**
+     * @return the sd
+     */
+    @Override
+    public Double getSd()
+    {
+        return this.sd;
+    }
+
+    /**
+     * @param sd the sd to set
+     */
+    public void setSd(Double sd)
+    {
+        this.sd = sd;
+    }
+    
+    /**
+     * @return the p10
+     */
+    @Override
+    public Double getP10()
+    {
+        return this.p10;
+    }
+
+    /**
+     * @param p10 the p10 to set
+     */
+    public void setP10(Double p10)
+    {
+        this.p10 = p10;
+    }
+    
+    /**
+     * @return the p25
+     */
+    @Override
+    public Double getP25()
+    {
+        return this.p25;
+    }
+
+    /**
+     * @param p25 the p25 to set
+     */
+    public void setP25(Double p25)
+    {
+        this.p25 = p25;
+    }
+    
+    /**
+     * @return the p50
+     */
+    @Override
+    public Double getP50()
+    {
+        return this.p50;
+    }
+
+    /**
+     * @param p50 the p50 to set
+     */
+    public void setP50(Double p50)
+    {
+        this.p50 = p50;
+    }
+    
+    /**
+     * @return the p75
+     */
+    @Override
+    public Double getP75()
+    {
+        return this.p75;
+    }
+
+    /**
+     * @param p75 the p75 to set
+     */
+    public void setP75(Double p75)
+    {
+        this.p75 = p75;
+    }
+    
+    /**
+     * @return the p90
+     */
+    @Override
+    public Double getP90()
+    {
+        return this.p90;
+    }
+
+    /**
+     * @param p90 the p90 to set
+     */
+    public void setP90(Double p90)
+    {
+        this.p90 = p90;
+    }
+    
+    /**
+     * @return the gtBelowMean
+     */
+    public Double getGtBelowMean()
+    {
+        return this.gtBelowMean;
+    }
+
+    /**
+     * @param gtBelowMean the gtBelowMean to set
+     */
+    public void setGtBelowMean(Double gtBelowMean)
+    {
+        this.gtBelowMean = gtBelowMean;
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (this.ageYears == null ? 0 : this.ageYears.hashCode());
+        result = prime * result + (this.mean == null ? 0 : this.mean.hashCode());
+        result = prime * result + (this.meanAge == null ? 0 : this.meanAge.hashCode());
+        result = prime * result + (this.p10 == null ? 0 : this.p10.hashCode());
+        result = prime * result + (this.p25 == null ? 0 : this.p25.hashCode());
+        result = prime * result + (this.p50 == null ? 0 : this.p50.hashCode());
+        result = prime * result + (this.p75 == null ? 0 : this.p75.hashCode());
+        result = prime * result + (this.p90 == null ? 0 : this.p90.hashCode());
+        result = prime * result + (this.sample == null ? 0 : this.sample.hashCode());
+        result = prime * result + (this.sd == null ? 0 : this.sd.hashCode());
+        result = prime * result + (this.gtBelowMean == null ? 0 : this.gtBelowMean.hashCode());
+        result = prime * result + (this.mdbmiageinfId == null ? 0 : this.mdbmiageinfId.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof MDbmiageinf))
+            return false;
+        MDbmiageinf other = (MDbmiageinf) obj;
+        if (this.ageYears == null) {
+            if (other.ageYears != null)
+                return false;
+        } else if (!this.ageYears.equals(other.ageYears))
+            return false;
+        if (this.mean == null) {
+            if (other.mean != null)
+                return false;
+        } else if (!this.mean.equals(other.mean))
+            return false;
+        if (this.meanAge == null) {
+            if (other.meanAge != null)
+                return false;
+        } else if (!this.meanAge.equals(other.meanAge))
+            return false;
+        if (this.p10 == null) {
+            if (other.p10 != null)
+                return false;
+        } else if (!this.p10.equals(other.p10))
+            return false;
+        if (this.p25 == null) {
+            if (other.p25 != null)
+                return false;
+        } else if (!this.p25.equals(other.p25))
+            return false;
+        if (this.p50 == null) {
+            if (other.p50 != null)
+                return false;
+        } else if (!this.p50.equals(other.p50))
+            return false;
+        if (this.p75 == null) {
+            if (other.p75 != null)
+                return false;
+        } else if (!this.p75.equals(other.p75))
+            return false;
+        if (this.p90 == null) {
+            if (other.p90 != null)
+                return false;
+        } else if (!this.p90.equals(other.p90))
+            return false;
+        if (this.sample == null) {
+            if (other.sample != null)
+                return false;
+        } else if (!this.sample.equals(other.sample))
+            return false;
+        if (this.sd == null) {
+            if (other.sd != null)
+                return false;
+        } else if (!this.sd.equals(other.sd))
+            return false;
+        if (this.gtBelowMean == null) {
+            if (other.gtBelowMean != null)
+                return false;
+        } else if (!this.gtBelowMean.equals(other.gtBelowMean))
+            return false;
+        if (this.mdbmiageinfId == null) {
+            if (other.mdbmiageinfId != null)
+                return false;
+        } else if (!this.mdbmiageinfId.equals(other.mdbmiageinfId))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "MDbmiageinf [mdbmiageinfId=" + this.mdbmiageinfId + ", ageYears=" + this.ageYears + ", meanAge=" + this.meanAge
+                + ", sample=" + this.sample + ", mean=" + this.mean + ", sd=" + this.sd + ", p10=" + this.p10 + ", p25=" + this.p25 + ", p50=" + this.p50
+                + ", p75=" + this.p75 + ", p90=" + this.p90 + ", gtBelowMean=" + this.gtBelowMean + "]";
+    }
+    
+}
---
+++ api/src/main/ruleLibrary/java rules/CalculateMDBMIPercentile.java
@@ -0,0 +1,126 @@
+package org.openmrs.module.chica.rule;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.Obs;
+import org.openmrs.Patient;
+import org.openmrs.api.context.Context;
+import org.openmrs.logic.LogicContext;
+import org.openmrs.logic.LogicException;
+import org.openmrs.logic.Rule;
+import org.openmrs.logic.result.Result;
+import org.openmrs.logic.result.Result.Datatype;
+import org.openmrs.logic.rule.RuleParameterInfo;
+import org.openmrs.module.chica.Calculator;
+import org.openmrs.module.chirdlutil.util.ChirdlUtilConstants;
+
+/**
+ * Calculates bmi percentile based upon a provided weight, the patient's age, and gender.
+ * 
+ * @author Seema Sarala
+ */
+public class CalculateMDBMIPercentile implements Rule {
+    
+    private static final String CALCULATION_MD_BMI_PERCENTILE = "mdbmi";
+    
+    private Log log = LogFactory.getLog(this.getClass());
+    
+    /**
+     * @see org.openmrs.logic.Rule#eval(org.openmrs.logic.LogicContext, java.lang.Integer, java.util.Map)
+     */
+    @Override
+    public Result eval(LogicContext context, Integer patientId, Map<String, Object> parameters) throws LogicException {
+        if (parameters == null || parameters.isEmpty()) {
+            return Result.emptyResult();
+        }
+        
+        // Ensure the parameter is a Result
+        Object weightResultsObject = parameters.get(ChirdlUtilConstants.PARAMETER_1);
+        if (!(weightResultsObject instanceof Result)) {
+            return Result.emptyResult();
+        }
+        
+        Result weightResults = (Result)weightResultsObject;
+        
+        // Ensure the patient exists
+        Patient patient = Context.getPatientService().getPatient(patientId);
+        if (patient == null) {
+            this.log.error("Cannot find patient with ID " + patientId);
+            return Result.emptyResult();
+        }
+        
+        // Ensure the patient has a birthdate
+        Date birthDate = patient.getBirthdate();
+        if (birthDate == null) {
+            this.log.error("Patient " + patientId + " does not have a birthdate specified.");
+            return Result.emptyResult();
+        }
+        
+        Double weight = null;
+        Date dateTime = null;
+        // Get the weight observation
+        Object weightObsObj = weightResults.getResultObject();
+        if (weightObsObj instanceof Obs) {
+            Obs weightObs = (Obs)weightObsObj;
+            weight = weightObs.getValueNumeric();
+            dateTime = weightObs.getObsDatetime();
+        } else if (Datatype.NUMERIC.equals(weightResults.getDatatype())) {
+            weight = weightResults.toNumber();
+            dateTime = weightResults.getResultDate();
+        } else {
+            return Result.emptyResult();
+        }
+        
+        if (weight == null) {
+            return Result.emptyResult();
+        }
+        
+        // Calculate the weight percentile
+        Calculator calc = new Calculator();
+        try {
+            Double weightPercentile = calc.calculatePercentile(weight, birthDate, CALCULATION_MD_BMI_PERCENTILE, dateTime);
+            return new Result(weightPercentile);
+        } catch (Exception e) {
+            this.log.error("Error calculating weight percentile for patient " + patientId, e);
+            return Result.emptyResult();
+        }
+    }
+    
+    /**
+     * @see org.openmrs.logic.Rule#getParameterList()
+     */
+    @Override
+    public Set<RuleParameterInfo> getParameterList() {
+        return new HashSet<>();
+    }
+    
+    /**
+     * @see org.openmrs.logic.Rule#getDependencies()
+     */
+    @Override
+    public String[] getDependencies() {
+        return new String[0];
+    }
+    
+    /**
+     * @see org.openmrs.logic.Rule#getTTL()
+     */
+    @Override
+    public int getTTL() {
+        return 0;
+    }
+    
+    /**
+     * @see org.openmrs.logic.Rule#getDefaultDatatype()
+     */
+    @Override
+    public Datatype getDefaultDatatype() {
+        return Datatype.NUMERIC;
+    }
+    
+}
